8e005ffc8d5a940a6110f99c1958f8677aedf938
krlvn <krlvn39@gmail.com>
2022-09-18 15:06:25 +0200

    Вьюшки главной страницы и страницы статьи (без шаблонов).

--- django/articles/views.py
+++ django/articles/views.py
@@ -1,8 +1,26 @@
-# from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.urls import reverse
+
+from .models import Article
 
 def home_view(request):
-    return HttpResponse('Это главная страница со список последних статей.')
+    # Ищем все статьи со статусом 2 (Опубликовано) и сортируем по дате создания.
+    articles = Article.objects.filter(status=2).order_by('created_at')
+
+    # Эту строку раскомментить, когда будет готов шаблон.
+    # return render(request, 'articles_template.html', {'articles': articles})
+    # А эту удалить:
+    response = HttpResponse()
+    for article in articles:
+        response.write(f'''<a href="{reverse('article', args=[article.id])}">{article.title}</a><br />''')
+    return response
 
 def article_view(request, id):
-    return HttpResponse(f'Это страница статьи с id={id}')
+    # Ищем статью по id со статусом 2 (Опубликовано).
+    article = get_object_or_404(Article, pk=id, status=2)
+
+    # Эту строку раскомментить, когда будет готов шаблон.
+    # return render(request, 'article_template.html', {'article': article})
+    # А эту удалить:
+    return HttpResponse(f'''<h1>{article.title}</h1>{article.text}''')

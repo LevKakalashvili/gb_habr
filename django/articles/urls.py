14e8c6c7dea50f4748854e4e7cf14e3e92a843da
krlvn <krlvn39@gmail.com>
2022-09-18 15:05:22 +0200

    Маршрутизация к главной странице, и к странице статьи.

--- django/articles/urls.py
+++ django/articles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import home_view, article_view
 
 urlpatterns = [
-    path('article/<int:id>/', article_view, name='article'),
+    path('article-<int:id>/', article_view, name='article'),
     path('', home_view)
 ]

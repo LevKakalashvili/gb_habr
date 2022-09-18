446c31b39b214e5df5ab7f099698940c3356fc49
krlvn <krlvn39@gmail.com>
2022-09-18 14:14:07 +0200

    Маршрутизация к главной странице, и к странице статьи.

--- django/articles/views.py
+++ django/articles/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+def home_view(request):
+    return HttpResponse('Это главная страница со список последних статей.')
+
+def article_view(request, id):
+    return HttpResponse(f'Это страница статьи с id={id}')
